6c6a036f8e19a7162f07e27668376c6149674244
korgn <69055432+korgn@users.noreply.github.com>
2023-03-14 21:48:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ def handle_message(message):
     bot.forward_message(admin_id[2], message.chat.id, message.message_id)
     
     # Send checkmark emoji as reply to the user's message
-    bot.send_message(chat_id=message.chat.id, text='✅')
+    bot.send_message(chat_id=message.chat.id, text='☑️')
 
 bot.polling(none_stop=True)
 
